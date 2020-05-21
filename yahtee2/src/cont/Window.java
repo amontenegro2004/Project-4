96a6050c883b43947ca4835883168a2f82fab390
amontenegro2004 <65563129+amontenegro2004@users.noreply.github.com>
2020-05-21 10:19:06 -0400

    Update Window.java

--- yahtee2/src/cont/Window.java
+++ yahtee2/src/cont/Window.java
@@ -3,15 +3,23 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class Window {
-
+	/* Method used to display a msg of a dialog boxe with a message
+	*/
 	public void msg(String msg) {
 		JOptionPane.showMessageDialog(null, msg);
 	}
 	
+	/* Method used to display a a dialog box with input and displays
+	* a message.
+	*/
 	public String in(String msg) {
 		return JOptionPane.showInputDialog(msg);
 	}
 	
+	/* Method that shows a dialog box with different options,
+	* that take in a string of options for the buttons and displays
+	* a message.
+	*/
 	public int option(String[] options, String msg) {
 		return JOptionPane.showOptionDialog(
 				null, 
@@ -24,22 +32,33 @@ public class Window {
                 options[0]); // default option
 	}
 	
+	/* Method used to print out a line of message in the system output.
+	*/
 	public void println(String msg) {
 		System.out.println(msg);
 	}
 	
+	/* Method used to print out message in the system output all on the same line.
+	*/
 	public void print(String msg) {
 		System.out.println(msg);
 	}
 	
+	/* Method used to display a msg of a dialog box with a msg that is an object
+	*/
 	public void msg(Object msg) {
 		JOptionPane.showMessageDialog(null, msg);
 	}
-	
+	 
+	/* Method used to display a msg with input that the msg is an object
+	*/
 	public String in(Object msg) {
 		return JOptionPane.showInputDialog(msg);
 	}
 	
+	/* Method used to display an option box, with an array of object options,
+	* an object message, and a string.
+	*/
 	public int option(Object[] options, Object msg, String string) {
 		return JOptionPane.showOptionDialog(
 				null, 
